915e14978eb4deabf2f8977ffc064f59529ba2be
vidu1999 <78868155+vidu1999@users.noreply.github.com>
2023-04-08 19:51:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Vidura1999" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "NmRHNEhDSk4=" //session Id
+global.session = "azJZTUU4Z24=" //session Id
 global.ownernomer = "94781031038" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94781031038'] //ur premium number
